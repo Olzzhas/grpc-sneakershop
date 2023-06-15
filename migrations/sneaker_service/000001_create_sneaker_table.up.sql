eaeaa9be5eeeabc492510e4f18cc57f46b357a33
MenkinaSabina <you@example.com>
2023-06-15 06:50:17 +0600

    migration for sneakers

---
+++ migrations/sneaker_service/000001_create_sneaker_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS sneakers (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    size FLOAT NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+    );
